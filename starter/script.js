011c7eddbc715fea42cd29ca50ba0eccaff6af07
syed <novelcode0@gmail.com>
2021-05-19 08:37:45 +0530

    lec 191 Tabbed Component

--- starter/script.js
+++ starter/script.js
@@ -84,7 +84,35 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
-}); /*
+});
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+// tabs.forEach(t => t.addEventListener('click', () => console.log('tab'))); (bad practice makes page slow)
+
+/*
 
 /*
 // 185 Implementing Smooth Scrolling
