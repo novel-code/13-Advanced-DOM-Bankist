a5e59464d1cdb8048839fbb8bc6e8509ca417e86
syed <novelcode0@gmail.com>
2021-05-19 12:31:04 +0530

    lec 194 Intersection Observer API

--- starter/script.js
+++ starter/script.js
@@ -127,20 +127,52 @@ const handleHover = function (e) {
 };
 
 // Passing "argument" into handler
-nav.addEventListener('mouseover', handleHover.bind(0.5));
+// nav.addEventListener('mouseover', handleHover.bind(0.5));
 
-nav.addEventListener('mouseout', handleHover.bind(1));
+// nav.addEventListener('mouseout', handleHover.bind(1));
 
-// Sticky navigation
-const initialCoords = section1.getBoundingClientRect();
-// console.log(initialCoords);
+// // Sticky navigation
+// const initialCoords = section1.getBoundingClientRect();
+// // console.log(initialCoords);
 
-window.addEventListener('scroll', function () {
-  // console.log(window.scrollY);
+// window.addEventListener('scroll', function () {
+//   // console.log(window.scrollY);
 
-  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+// lec 194 A Better Way: The Intersection Observer API
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
 
 /*
 nav.addEventListener('mouseover', function (e) {
